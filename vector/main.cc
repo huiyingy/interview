0bc1f2a62ab8a2a364ac80bca6cae9b41a351fd2
december23 <and_december@sina.com>
2017-02-25 12:12:16 +0800

    complete vector and do some tests

--- vector/main.cc
+++ vector/main.cc
@@ -1,14 +1,96 @@
-#include <stdlib.h>
 #include "qh_vector.h"
 
+#include <cstdlib>
+#include <cassert>
+#include <cstdio>
+#include <climits>
+
+void test1()
+{
+
+    qh::vector<int> v(2,9);
+    assert(v.size() == 2);
+   	
+   	v.push_back(1);
+    assert(v.size() == 3);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    assert(v.size() == 6);
+    
+    /* for (size_t i = 0; i < v.size(); ++i)
+    {
+    	printf("%d ",v[i] );
+    }printf("\n");*/
+    v.push_back(5);
+
+    v.pop_back();
+    v.pop_back();
+    assert(v.size() == 5);
+    v.clear();
+	assert(v.empty());
+}
+
+void test2(){
+	qh::vector<int> v;
+	try{
+		v.pop_back();
+	}
+	catch(int n){
+		printf("oops\n");
+	}
+	if(v.empty()){
+		v.resize(6);
+		assert(v.size()==6);
+
+		v[4 ] = INT_MAX;
+		v.pop_back();
+		v[4] ++;
+		assert(v[4] ==INT_MIN );
+
+		/*for (int i = 0; i < 6; ++i)
+		{
+			printf("%d ",v[i] );
+		}printf("\n");*/
+	}
+}
+
+void test3(){
+	qh::vector<char> v;
+	v.reserve(1233333);
+	assert(v.size()==0 );
+
+	v.resize(10,'5');
+	assert(v[2]=='5');
+
+	v.push_back('~');
+	v.push_back('\n');
+	v.push_back('\t');
+	v.push_back(',');
+	assert(v.size()==14 );
+
+	qh::vector<char> v2(12,'@');
+	assert(v2[0]='@' );
+	assert(v2[11]='@' );
+	assert(v2.size()==12 );
+
+	v = v2;
+	assert(v.size()==12 );
+	assert(v[3]=='@' );
+
+	v.clear();
+	assert(v.empty() );
+
+}
 
 int main(int argc, char* argv[])
 {
     //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
     //TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
+test1();
+test2();
+test3();
 
-    qh::vector<int> num_vect;
-    num_vect.push_back(1);
 
 #ifdef WIN32
     system("pause");
--- vector/qh_vector.h
+++ vector/qh_vector.h
@@ -1,6 +1,7 @@
 #ifndef QIHOO_VECTOR_H_
 #define QIHOO_VECTOR_H_
-
+#include <cstdio>
+#include <cassert>
 namespace qh
 {
     template<class T>
@@ -14,17 +15,35 @@ namespace qh
 
         explicit vector( size_t n, const T& value = T())
         {
-            data_ = new T[n];
+            capa_ = n* FACTOR;
+            if( capa_ < n+2){
+                capa_ = n+2;
+            }
+
+            data_ = new T[capa_];
             for (size_t i = 0; i < n; i++)
             {
-                data_[0] = value;
+                data_[i] = value;
             }
+            size_ = n;
+
         }
 
         vector<T>& operator=(const vector<T>& rhs)
         {
-            data_ = rhs.data_;
+            if(data_ ){
+                delete data_;
+            }
+
+            data_ = new T[rhs.capa_];
             size_ = rhs.size_;
+            capa_ = rhs.capa_;
+            for (size_t i = 0; i < size_; ++i)
+            {
+                data_[i] = rhs.data_[i];
+            }
+
+            return *this;
         }
 
         //dtor
@@ -42,21 +61,108 @@ namespace qh
             return size_;
         }
 
+
+        T pop_back(){
+            if(size_!=0 ){
+                size_ --;
+                return data_[size_];
+            }
+            else throw 0;
+        }
+
+        void resize (size_t n, const T& value = T());
+        void clear(){size_ = 0;};
+        bool empty() {return size_==0;}
+
+
         // set & get
         T& operator[](size_t index);
 
         // set
-        void push_back(const T& element);
-        void pop_back(const T& element);
-        void resize();
-        void reserve();
-        void clear();
-        void empty();
-
+        void push_back(const T & element);
+        void reserve(size_t n);
     private:
         T*      data_;
         size_t  size_;
+        size_t  capa_;//数组容量
+        const static double FACTOR;//数组生长因子
+
+        void aux_extend(size_t  new_capa );//扩容辅助函数
     };
+
+    template<class T>
+    T& vector<T>::operator[](size_t index){
+            if(index<size_ )
+                return data_[index];
+            else
+                throw index;
+        };
+
+    template<class T>
+    void vector<T>::push_back(const T & element){
+            if(capa_ == size_ ){
+                int tp = (capa_*FACTOR) > size_ + 2 ? (capa_*FACTOR): size_+2;
+                aux_extend(tp);
+            }
+
+            data_[size_++] = element;
+            return ;
+        };
+
+    template<class T>
+    void vector<T>::aux_extend(size_t  new_capa ){
+            T* tmp = data_;
+            data_ = new T[new_capa];
+            for (size_t i = 0; i < size_; ++i){
+                data_[i] = tmp[i];
+            }
+
+            if (tmp){
+                delete tmp;
+            }
+            
+            capa_ = new_capa;
+        };//扩容辅助函数
+
+    template<class T>
+    const double vector<T>::FACTOR = 1.5;       
+
+    template<class T>
+    void vector<T>::resize (size_t n, const T& value ){
+/*      if(n<0 )
+            return;*/
+
+        if(n <=size_ ){
+            size_ = n;
+        }
+        else{
+            if(n>capa_ ){
+                int tp = n;
+                if( (capa_*FACTOR) > n)
+                    tp = (capa_*FACTOR);
+                 
+                aux_extend(tp);
+            }
+
+            for (size_t i = size_; i < n; ++i){
+                data_[i] = value;
+            }
+            size_ = n;
+        }
+        return;
+    }
+
+
+    template<class T>
+    void vector<T>::reserve(size_t n){
+        if(n<=capa_ )
+            return;
+
+        int tp = ((capa_*FACTOR) > n) ? (int)(capa_*FACTOR): n;
+        aux_extend(tp);
+
+    }
+
 }
 
 #endif
